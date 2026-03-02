41d04a63dd5380ecb38d4edd351dc15611e51cc3
leizov <karman98752@gmail.com>
2026-03-02 21:01:31 +0300

    1.0.29

--- __init__.py
+++ __init__.py
@@ -1,7 +1,7 @@
 from colorama import Fore
 
 # Seal Playerok Bot - милым быть круто 🦭
-VERSION = "1.0.28"
+VERSION = "1.0.29"
 DEVELOPER = "@leizov"
 REPOSITORY = "https://github.com/leizov/Seal-Playerok-Bot"
 SKIP_UPDATES = False
